edaccdc242f7e2f152c6ec5e99f8a6ffa06f771e
Gustavotxn <guhds320@gmail.com>
2025-05-15 05:48:05 -0300

    atualizacao exercicios01

--- exercicios01/src/exercicios01/Exercicios01.java
+++ exercicios01/src/exercicios01/Exercicios01.java
@@ -1,22 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicios01;
 
-/**
- *
- * @author aluno.saolucas
- */
-public class Exercicios01 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Teste");
+public class Exercicios01 {
+// Crie um método que receba um número inteiro e retorne true se for um número perfeito (a soma de 
+// seus divisores próprios é igual ao número, ex: 6 = 1 + 2 + 3).
+    
+   
+    
+    
+        
+        
+        
+    
+                
+        public class NumeroPerfeito {
+            
+        }
+
+    public static boolean NumeroPerfeito(int n) {
+    
+        if (n <= 1) {
+            return false;
+        }
+
+        int somaDivisores = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                somaDivisores += i;
+            }
+        }
+
+        return somaDivisores == n;
     }
     
+    public static void main(String[] args){
+    
+        System.out.println(NumeroPerfeito(6));  
+        System.out.println(NumeroPerfeito(21));  
+        System.out.println(NumeroPerfeito(12));  
+        
+        
+    
+    }
 }
+    
+   
+
+        
+        
+        
+        
+    
+    
+
