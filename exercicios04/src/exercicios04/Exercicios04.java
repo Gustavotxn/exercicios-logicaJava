916437720ca17667d315f8e60b982067bed5c249
Gustavotxn <guhds320@gmail.com>
2025-05-22 16:23:47 +0900

    finalizado

--- exercicios04/src/exercicios04/Exercicios04.java
+++ exercicios04/src/exercicios04/Exercicios04.java
@@ -1,21 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exercicios04;
 
-/**
- *
- * @author aluno.saolucas
- */
+
 public class Exercicios04 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+  
+     
+    public class StringComMaisVogais {
     }
-    
+    public static String stringComMaisVogais(String[] palavras) {
+        String resultado = "";
+        int maxVogais = 0;
+
+        for (String palavra : palavras) {
+            int contador = contarVogais(palavra);
+            if (contador > maxVogais) {
+                maxVogais = contador;
+                resultado = palavra;
+            }
+        }
+
+        return resultado;
+    }
+
+    private static int contarVogais(String palavra) {
+        int contador = 0;
+        String vogais = "aeiouAEIOU";
+
+        for (char c : palavra.toCharArray()) {
+            if (vogais.indexOf(c) != -1) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        String[] palavras = {"banana", "melancia", "kiwi", "abacaxi"};
+        String resultado = stringComMaisVogais(palavras);
+        System.out.println("String com mais vogais: " + resultado);
+    }
 }
+    
+        
+        
+        
+        
+        
+    
+    
+
